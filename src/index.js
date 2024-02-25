56df21aff0075ec31ac718763680d05acfccc254
ByBillia19 <KEAMAFORA19@GMAIL.COM>
2024-02-25 23:29:32 +0200

    addded functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
-function refreshWeather(response){
-    console.log(response.data);
-}
+function displayTemperature(response) {
+            let temperatureElement = document.querySelector("#current-temperature");
+            let temperature = Math.round(response.data.temperature.current);
+            let cityElement = document.querySelector("#city");
+            cityElement.innerHTML = response.data.city;
+            temperatureElement.innerHTML = temperature;
+        }
 
-function searchCity (city){
-let apiKey="bof6afef739a369a2ca570a747e3a1ct";
-let apiUrl='https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apyiKey}&units=metric';
-axios.get(apiUrl).then(refreshWeather);
-}
+        function search(event) {
+            event.preventDefault();
+            let searchInputElement = document.querySelector("#search-form-input");
+            let city = searchInputElement.value;
 
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value ;
-    searchCity(searchInput.value);
-}
-let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+            let apiKey = "b2a5adcct04b33178913oc335f405433";
+            let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+            axios.get(apiUrl).then(displayTemperature);
+        }
+
+        function formatDate(date) {
+            let minutes = date.getMinutes();
+            let hours = date.getHours();
+            let day = date.getDay();
+
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+
+            if (hours < 10) {
+                hours = `0${hours}`;
+            }
+
+            let days = [
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+            ];
+
+            let formattedDay = days[day];
+            return `${formattedDay} ${hours}:${minutes}`;
+        }
+
+        let searchForm = document.querySelector("#search-form");
+        searchForm.addEventListener("submit", search);
+
+        let currentDateELement = document.querySelector("#current-date");
+        let currentDate = new Date();
+
+        currentDateELement.innerHTML = formatDate(currentDate);
+    
